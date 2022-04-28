eee027b9eb2f7b50f081074a1b229295c95ac59c
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2022-04-28 11:38:18 +0700

    add gui

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from models.modelbrand_ads import Brand, Ads
 from package.brand_ads import Brands
+from tkinter import ttk
+from tkinter import *
+
 db = Brands()
-all_brand = db.brand().all_brands()
-print(all_brand)
+def show_all():
+    for item in db.brand().all_brands():
+        lis.insert(END, item)
+
+root = Tk()
+root.title("Brands")
+
+lis = Listbox(root, width=500, height=500)
+all_songs = Button(root, text="Show all",
+                   command=show_all).pack(side=BOTTOM)
+lis.pack()
+
+
+root.mainloop()
